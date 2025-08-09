47c0a48a0370cd9906869f622efbeb3a18f04957
bravoarthur <arthur.bravo08@gmail.com>
2025-08-09 23:59:15 +1000

    finished

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,7 @@ export default function Footer() {
       <div className="max-w-5xl mx-auto px-4 text-center">
         <p>&copy; {new Date().getFullYear()} AB Green Garden Maintenance. All rights reserved.</p>
         <p>Contact us: <a href="mailto:quote.gcare@gmail.com" className="underline">quote.gcare@gmail.com</a></p>
+        <p>Phone: 0424 226 446</p>
         <div className="mt-4 flex justify-center space-x-4">
           <a href="https://www.facebook.com/profile.php?id=61563579015089" className="text-white hover:text-light-garden">Facebook</a>
           <a href="https://wa.me/+610424226446" className="text-white hover:text-light-garden">WhatsApp</a>
